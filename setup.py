d7105473d874bcdd1e7035ce7478adb7a7c01f25
Michael Hartle <mhartle@hartle-klug.com>
2024-05-29 14:41:24 +0200

    Added passing-through of CMAKE_CUDA_ARCHITECTURES

--- setup.py
+++ setup.py
@@ -37,6 +37,7 @@ class CMakeBuild(build_ext):
     def build_extension(self, ext):
         extdir = os.path.abspath(os.path.dirname(self.get_ext_fullpath(ext.name)))
         cmake_args = [
+            '-DCMAKE_CUDA_ARCHITECTURES='+os.environ.get('CMAKE_CUDA_ARCHITECTURES'),
             '-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=' + extdir,
             '-DPYTHON_EXECUTABLE=' + sys.executable,
             '-DVERSION_INFO={}'.format(self.distribution.get_version()),
